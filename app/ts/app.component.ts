1f74d47163a8e55a7281460823bde8a0221c3143
magicodeuk <bailey.jim@outlook.com>
2017-01-13 00:09:11 +0000

    Added video player component

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -1,17 +1,19 @@
 import {Component} from "angular2/core";
 import {Config} from "./config.service";
 import {Video} from "./video";
+import {VideoPlayerComponent} from "./videoplayer.component";
 import {PlaylistComponent} from "./playlist.component";
 
 @Component({
     selector: "my-app",
     templateUrl: "app/templates/app.component.html",
-    directives: [PlaylistComponent]
+    directives: [PlaylistComponent, VideoPlayerComponent]
 })
 
 export class AppComponent {
     mainHeading = Config.MAIN_HEADING;
     videos: Array<Video>;
+    video: Video;
 
     constructor() {
         this.videos = [
@@ -19,5 +21,10 @@ export class AppComponent {
             new Video(2, "A Beautiful Mind", "WFJgUm7iOKw", "After John Nash, a brilliant but asocial mathematician, accepts secret work in cryptography, his life takes a turn for the nightmarish."),
             new Video(3, "The Truman Show", "loTIzXAS7v4", "An insurance salesman/adjuster discovers his entire life is actually a television show.")
         ];
+        this.video = this.videos[0];
+    }
+
+    onVideoSelected(video: Video) {
+        this.video = video;
     }
 }
--- app/ts/playlist.component.ts
+++ app/ts/playlist.component.ts
@@ -1,14 +1,23 @@
-import {Component} from 'angular2/core';
+import {Component, Input, Output, EventEmitter} from 'angular2/core';
 import {Video} from "./video";
 
 @Component({
     selector: "playlist",
-    templateUrl: "app/templates/playlist.component.html",
-    inputs: ['videos']
+    templateUrl: "app/templates/playlist.component.html"
 })
 
-export class PlaylistComponent{
-    onSelect(vid:Video) {
-        console.log(vid);
+export class PlaylistComponent {
+    @Input() videos: Array<Video>;
+    @Output() onSelectedVideo = new EventEmitter<Video>();
+    currentVideo: Video;
+
+    // Not really necessary but I don't want it to be null at the beginning so I set it to the first one in the list
+    ngOnInit() {
+        this.currentVideo = this.videos[0];
+    }
+
+    onSelect(video:Video) {
+        this.onSelectedVideo.emit(video);
+        this.currentVideo = video;
     }
 }
